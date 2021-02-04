c121ebacdc466f2d4c6a76f8856f53aa3544c2a1
Herman <herman.bastesen@outlook.com>
2021-02-04 11:17:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WarSimulator
+{
+    class Program
+    {
+        static int gameCount;
+        static void Main(string[] args)
+        {
+            try
+            {
+                gameCount = int.Parse(args[0]);
+            } catch (Exception e)
+            {
+                if (e is FormatException || e is IndexOutOfRangeException)
+                    Usage();
+                throw;
+            }
+
+            Console.WriteLine("Starting simulation of {0} games.", gameCount);
+            Simulator simulator = new Simulator(gameCount);
+            simulator.Simulate();
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage: WarSimulator.exe <n games>");
+            System.Environment.Exit(1);
+        }
+    }
+}
---
+++ Simulator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarSimulator
+{
+    class Simulator
+    {
+        private int gameCount;
+        private Random random = new Random();
+
+        //Game variables
+        private byte[] p1Hand;
+        private byte[] p2Hand;
+        public Simulator(int gameCount)
+        {
+            this.gameCount = gameCount;
+        }
+        public void Simulate()
+        {
+            for (int g = 0; g < gameCount; g++)
+            {
+                InitGame();
+                Console.WriteLine("[{0}]", string.Join(", ", p1Hand));
+                Console.WriteLine("[{0}]", string.Join(", ", p2Hand));
+            }
+        }
+
+        private void InitGame()
+        {
+            p1Hand = new byte[52];
+            p2Hand = new byte[52];
+
+            byte[] deck = new byte[52];
+            int c = 0;
+            //Fill deck with correct value cards
+            for (byte s = 0; s < 4; s++)
+            {
+                for (byte n = 1; n < 14; n++)
+                {
+                    deck[c++] = n;
+                }
+            }
+
+            //Shuffle (fisher-yates)
+            int j;
+            for (int i = 51; i > 0; i--)
+            {
+                j = random.Next(i);
+                byte t = deck[i];
+                deck[i] = deck[j];
+                deck[j] = t;
+            }
+
+            //Deal
+            for (int i = 0; i < 26; i++)
+            {
+                p1Hand[i] = deck[i];
+                p2Hand[i] = deck[26 + i];
+            }
+        }
+    }
+}
